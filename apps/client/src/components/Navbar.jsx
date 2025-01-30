641256c7ca5af6b48bbbd5d67cdf6d237d168dc1
Joel <joeldome17@gmail.com>
2025-01-29 20:40:32 -0600

    fix: responsive navbar component

--- apps/client/src/components/Navbar.jsx
+++ apps/client/src/components/Navbar.jsx
@@ -1,32 +1,54 @@
 import { Link } from 'react-router';
 import logo from '../assets/images/juniolandia-logo.svg';
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return(
-    <>
-      <nav className="bg-white shadow-md py-4 px-6 flex justify-between items-center">
-      <div className="flex items-center space-x-2">
+    <nav className='bg-white shadow-md py-4 px-6 relative flex justify-between items-center'>
+      <div className='flex items-center space-x-2'>
         <Link to="/">
-          <img src={logo} alt="Juniorlandia Logo" className="h-10 w-auto" />
+          <img src={logo} alt='Juniorlandia Logo' className='h-9 w-auto' />
         </Link>
       </div>
-      <div className="flex space-x-6">
-        <Link to="/" style={{color: "var(--purple)"}} className="hover:underline">Home</Link>
-        <Link to="/categorias" style={{color: "var(--purple)"}} className="hover:underline">Categorías</Link>
-        <Link to="/explorar" style={{color: "var(--purple)"}} className="hover:underline">Explorar</Link>
-        <Link to="/sobre-nosotros" style={{color: "var(--purple)"}} className="hover:underline">Sobre nosotros</Link>
+      <div className='lg:hidden z-50' onClick={toggleMenu}>
+        {
+          isMenuOpen 
+            ? (<X className='h-6 w-6 text-[var(--purple)] transition-transform transform rotate-90 duration-300' />)
+            : (<Menu className='h-6 w-6 text-[var(--purple)] transition-transform transform duration-300' />)
+        }
       </div>
-      <div className="flex space-x-4">
-        <a href="/#" className="text-white px-4 py-2 rounded-lg hover:bg-[var(--purple-hover)] bg-[var(--purple)] transform transition-transform active:scale-90">
+      <div className={`fixed lg:static lg:flex inset-0 bg-white lg:bg-transparent flex flex-col lg:flex-row items-center justify-center lg:justify-end space-y-6 lg:space-y-0 lg:space-x-6 z-40 transition-transform duration-300 transform
+        ${
+          isMenuOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
+        }`
+      }>
+        {
+          isMenuOpen && (
+            <div className='lg:hidden flex flex-col items-center'>
+              <img src={logo} alt='Juniorlandia Logo' className='h-8 w-auto mb-4' />
+            </div>
+          )
+        }
+        <Link to='/' style={{color: 'var(--purple)'}} className='text-lg lg:text-base hover:underline' onClick={() => setIsMenuOpen(false)}>Home</Link>
+        <Link to='/categorias' style={{color: 'var(--purple)'}} className='text-lg lg:text-base hover:underline' onClick={() => setIsMenuOpen(false)}>Categorías</Link>
+        <Link to='/explorar' style={{color: 'var(--purple)'}} className='text-lg lg:text-base hover:underline' onClick={() => setIsMenuOpen(false)}>Explorar</Link>
+        <Link to='/sobre-nosotros' style={{color: 'var(--purple)'}} className='text-lg lg:text-base hover:underline' onClick={() => setIsMenuOpen(false)}>Sobre nosotros</Link>
+      
+        <a href='/#' className='text-white px-4 py-2 rounded-lg hover:bg-[var(--purple-hover)] bg-[var(--purple)] transform transition-transform active:scale-90' onClick={() => setIsMenuOpen(false)}>
           Iniciar sesión
         </a>
-        <a href="/#" className="text-white px-4 py-2 rounded-lg hover:bg-[var(--blue-hover)] bg-[var(--blue)] transform transition-transform active:scale-90">
+        <a href='/#' className='text-white px-4 py-2 rounded-lg hover:bg-[var(--blue-hover)] bg-[var(--blue)] transform transition-transform active:scale-90' onClick={() => setIsMenuOpen(false)}>
           Regístrate
         </a>
       </div>
     </nav>
-    </>
   )
 }
-
 export default Navbar;
