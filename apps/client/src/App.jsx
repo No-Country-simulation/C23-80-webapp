2472e2e5024cb6fa5dfd0319d4d6000fc70407f5
Joel <joeldome17@gmail.com>
2025-01-31 01:27:44 -0600

    feat: route creation

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import Home from './components/Home'
-import Card from './components/Card'
+import Home from './Home'
+import Navbar from './components/Navbar'
+import AboutUs from './AboutUs'
+import Explore from './Explore'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route index path='/' element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path='/card' element={<Card />} />
+        <Route path='/explorar' element={<Explore />} />
+        <Route path='/sobre-nosotros' element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   )
