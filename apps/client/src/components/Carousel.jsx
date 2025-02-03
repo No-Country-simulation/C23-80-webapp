a43312f78304c02cd7ce938df1e4ec2026f065c4
Joel <joeldome17@gmail.com>
2025-02-03 01:03:00 -0600

    fix: restructuring the component carousel to allow receiving an element or an array of elements

--- apps/client/src/components/Carousel.jsx
+++ apps/client/src/components/Carousel.jsx
@@ -1,16 +1,9 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useState } from "react";
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useState } from 'react';
 
-const slides = [
-  {title: 'Frontend', image: 'https://picsum.photos/1000/500?random=1'},
-  {title: 'Backend', image: 'https://picsum.photos/1000/500?random=2'},
-  {title: 'FullStack', image: 'https://picsum.photos/1000/500?random=3'},
-  {title: 'UI/UX', image: 'https://picsum.photos/1000/500?random=4'},
-]
-
-const Carousel = ({ isCarousel = true }) => {
+const Carousel = ({ isCarousel = true, data = [], title = '', image = '' }) => {
   const [currentImage, setCurrentImage] = useState(0);
-
+  const slides = isCarousel && data.length > 0 ? data : [{title, image}];
   const prevSlide = () => {
     setCurrentImage((prev) => (prev === 0 ? slides.length - 1 : prev - 1))
   }
@@ -20,30 +13,34 @@ const Carousel = ({ isCarousel = true }) => {
   }
 
   return(
-    <div className="relative w-full max-w-[1200px] mx-auto">
-      <div className="relative w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden rounded-lg shadow-lg">
-        <img 
-          src={slides[currentImage].image}
-          alt={slides[currentImage].title}
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute top-4 left-4 p-4 rounded-2xl bg-black/70">
-          <h2 style={{color: "var(--purple)"}} className="text-lg md:text-2xl lg:text-4xl font-bold">{slides[currentImage].title}</h2>
+    <div className='relative w-full max-w-[1200px] mx-auto'>
+      {slides.length > 0 && slides[currentImage]?.featuredImage ? (
+        <div className='relative w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden rounded-lg shadow-lg'>
+          <img 
+            src={slides[currentImage].featuredImage.secure_url}
+            alt={slides[currentImage].title}
+            className='w-full h-full object-cover'
+          />
+          <div className='absolute top-4 left-4 p-4 rounded-2xl bg-black/70'>
+            <h2 style={{color: 'var(--purple)'}} className='text-lg md:text-2xl lg:text-4xl font-bold'>{slides[currentImage].title}</h2>
+          </div>
         </div>
-      </div>
+      ) : (
+        <p className='text-center text-gray-500'>No hay datos disponibles ...</p>
+      )}
 
       {
-        isCarousel && (
+        isCarousel && slides.length > 1 && (
           <>
             <button
               onClick={prevSlide}
-              className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-3 rounded-full shadow-md hover:bg-gray-900"
+              className='absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-3 rounded-full shadow-md hover:bg-gray-900'
             >
               <ChevronLeft size={30} />
             </button>
             <button
               onClick={nextSlide}
-              className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-3 rounded-full shadow-md hover:bg-gray-900"
+              className='absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-3 rounded-full shadow-md hover:bg-gray-900'
             >
               <ChevronRight size={30} />
             </button>
