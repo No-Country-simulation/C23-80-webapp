29a2bb635dd4d3977f1af57d667c5e0fe25e19ed
Joel <joeldome17@gmail.com>
2025-02-04 17:54:51 -0600

    fix: newscard component allows to display image and title of the resource of each category

--- apps/client/src/components/NewsCard.jsx
+++ apps/client/src/components/NewsCard.jsx
@@ -1,17 +1,28 @@
 import {Star, Bookmark} from 'lucide-react'
 
-const NewsCard = () => {
+const NewsCard = ({ data }) => {  
   const handleClic = () =>{    
   }
 
   return(
     <div className='bg-gray-200 min-w-[250px] max-w-sm w-full rounded-lg shadow-md overflow-hidden flex-shrink-0'>
-      <div className='h-40 relative flex items-start space-x-2 p-2'>
-        <Star className='cursor-pointer active:scale-90 transition-transform duration-300' />
-        <Bookmark className='cursor-pointer active:scale-90 transition-transform duration-300' />
+      <div className='h-40 relative bg-gray-300'>
+        {data.image ? (
+          <img 
+            src={data.image} 
+            alt={data.title} 
+            className='absolute top-0 left-0 w-full h-full object-cover'
+          />
+        ) : (
+          <p className='text-gray-500 w-full flex justify-center items-center'>Sin imagen</p>
+        )}
+        <div className='relative z-10 flex space-x-2 p-2'>
+          <Star className='cursor-pointer active:scale-90 transition-transform duration-300 text-white' />
+          <Bookmark className='cursor-pointer active:scale-90 transition-transform duration-300 text-white' />
+        </div>
       </div>
-      <div className='flex items-center justify-between p-4 bg-purple-200 rounded-t-lg'>
-        <span className='text-lg text-gray-800 font-semibold'>Título</span>
+      <div className='flex items-center justify-between p-4 bg-purple-200'>
+        <span className='text-lg text-gray-800 font-semibold'>{data.title}</span>
         <button className='text-white text-sm rounded-md px-4 py-2 bg-purple-800 cursor-pointer active:scale-90 transition-transform duration-300' onClick={handleClic}>
           Ver más
         </button>
