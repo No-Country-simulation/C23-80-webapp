5d1eb7d6afae4e6d1c100818f857b81eb6cd2506
Joel <joeldome17@gmail.com>
2025-02-04 19:49:27 -0600

    feat: list of catgeories obtained from the api

--- apps/client/src/Categories.jsx
+++ apps/client/src/Categories.jsx
@@ -1,62 +1,54 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import NewsCard from "./components/NewsCard";
-import Carousel from "./components/Carousel";
-import SearchI from "./components/Search";
-import { useRef } from "react";
+import { useEffect, useState } from 'react';
+import { fetchCategories } from './Apis';
+import Card from './components/Card';
 
 const Categories = () => {
-  const scrollRef = useRef(null);
+  const [category, setCategory] = useState([]);
+  const [isCategoryLoading, setIsCategoryLoading] = useState(true);
 
-  const scrollLeft = () => {
-    scrollRef.current.scrollBy({
-      left: -400,
-      behavior: 'smooth',
-    });
-  };
-
-  const scrollRight = () => {
-    scrollRef.current.scrollBy({
-      left: 400,
-      behavior: 'smooth',
-    });
-  };
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categories = await fetchCategories();
+        if (Array.isArray(categories) && categories.length > 0) {
+          setCategory(categories);          
+        }
+      } catch (err) {
+        console.error('Error al obtener categoría:', err);
+      } finally {
+        setIsCategoryLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <div className='w-full flex flex-col items-center space-y-8 px-4 lg:px-8 my-10'>
-      <div className='w-full'>
-        <Carousel isCarousel = {false} />
-      </div>
-      <div className='w-full mt-6 flex justify-center'>
-        <SearchI />
-      </div>
-      <div className='w-full max-w-[1200px] relative mt-4'>
-        <h2 style={{color: 'var(--purple10)', font: 'var(--h2)'}} className='text-xl font-bold mb-4'>Recursos</h2>
-        <button
-          onClick={scrollLeft}
-          className='absolute left-3 top-1/2 -translate-y-1/2 bg-white/60 p-2 rounded-full shadow-md z-10'
-        >
-          <ChevronLeft className='h-6 w-6 text-[var(--purple)]'/>
-        </button>
-        <div ref={scrollRef} className='flex overflow-hidden space-x-4 py-2'>
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-        </div>
-        <button
-          onClick={scrollRight}
-          className='absolute right-3 top-1/2 -translate-y-1/2 bg-white/60 p-2 rounded-full shadow-md z-10'
-        >
-          <ChevronRight className='h-6 w-6 text-[var(--purple)]' />
-        </button>
+    <div className='w-full flex flex-col items-center space-y-8 px-4 lg:px-8 my-16'>
+      <div className='max-w-[1200px] w-full mx-auto'>
+        <h2 style={{color: 'var(--purple10)', font: 'var(--h1)'}} className='text-left font-bold mb-10'>Categorías</h2>
+        {isCategoryLoading ? (
+          <div className='flex justify-center items-center w-full'>
+            <div className='animate-spin rounded-full h-12 w-12 border-4 border-[var(--purple)] border-t-transparent'></div>
+          </div>
+        ) : (
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[1200px] w-full justify-items-center'>
+            {category.length > 0 ? (
+              category.map((cat) => {
+                const dataObject = {
+                  backgroundImage: cat.featuredImage.secure_url,
+                  title: cat.title,
+                };
+                return <Card key={cat.id} data={dataObject} />;
+              })
+            ) : (
+              <p className='text-center text-gray-500'>
+                No hay categorías disponibles...
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Categories;
--- apps/client/src/components/Card.jsx
+++ apps/client/src/components/Card.jsx
@@ -1,10 +1,14 @@
-const Card = ({ backgroundImage }) => {
-  return(
+const Card = ({ data }) => {
+  return (
     <div
-      className="rounded-lg shadow-lg bg-cover bg-center aspect-video max-w-[504px] w-full"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    ></div>
-  )
+      className='relative rounded-lg shadow-lg bg-cover bg-center aspect-video max-w-[504px] w-full'
+      style={{ backgroundImage: `url(${data.backgroundImage})` }}
+    >
+      <div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white px-3 py-1 rounded-lg">
+        <p style={{font: 'var(--h3)'}} className="text-base font-semibold capitalize">{data.title}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
